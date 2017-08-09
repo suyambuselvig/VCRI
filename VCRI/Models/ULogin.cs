fa8cb35b9d36d4a63e5444ec136a5bdd87ee20f1
Suyambu Selvi <suyambuselvig@gmail.com>
2017-08-09 22:46:05 +0530

    Initial Changes in Login & User Details

--- VCRI/Models/ULogin.cs
+++ VCRI/Models/ULogin.cs
@@ -11,19 +11,21 @@ namespace VCRI.Models
     public partial class ULogin
     {
 
-    
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int SNo { get; set; }
 
         [Required]
+        [Display(Name = "Login Name")]
         public string user_ID { get; set; }
         [Required]
+        [Display(Name = "User Name")]
         public string user_Name { get; set; }
         [Required]
+        [Display(Name = "Password")]
         public string user_Pwd { get; set; }
         [Required]
+        [Display(Name = "Role")]
         public string role { get; set; }
-
         [DefaultValue(true)]
         public bool IsActive { get; set; }
     }
